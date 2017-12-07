59877c1a57ffb45404a8f7b76dea43084a2eb19a
cxiao18 <34096914+cxiao18@users.noreply.github.com>
2017-12-07 12:10:14 -0500

    morse

---
+++ morsecode.py
@@ -0,0 +1,30 @@
+#Goals: practice github and classes
+
+#Messaging system: choose 3 outdated forms of messaging, write a class for each, write runner and filing classes
+	#Encrypted message, morse code, pig latin
+
+
+#Structure: def translate, return translation, write(translate to) and read(translate back)
+
+
+class MorseCode:
+	def __init__(self, message):
+		self.message = message
+	def encrypt(self, message):
+		encyrpted = [message]
+		encrypted[len.encrypted] 
+
+		a = morse[0]
+		b = morse[1]
+		c = morse[2]
+		d = morse[3]
+		e = morse[4]
+
+		morse = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..']
+		
+
+
+		#convert each letter into a numberical value
+		#use ord, minus ord of a,b,c,...
+
+ 
