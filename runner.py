39a3e80e604f024579104435a43dd02be1ab2582
cxiao18 <cxiao18@choate.edu>
2017-12-08 13:47:07 -0500

    a

--- runner.py
+++ runner.py
@@ -4,3 +4,9 @@ RSAtool = RSA()
 message = RSAtool.write("This is a message")
 print(message)
 print(RSAtool.read(message))
+
+from morsecode import MorseCode 
+
+MorseCodetool = MorseCode()
+message = MorseCodetool.encrypt()
+print(message)
