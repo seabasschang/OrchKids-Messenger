c3f3e7cedf706aa2c4a9437a9da09b34dc78b961
seabasschang <34096922+seabasschang@users.noreply.github.com>
2017-12-08 13:08:42 -0500

    Incomplete Pig Latin

--- piglatin.py
+++ piglatin.py
@@ -1,23 +1,31 @@
-# consonants = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"]
-# vowels = ["A", "E", "I", "O", "U"]
-def build(prompt)
-	dirty = str(input(''+prompt+' ')).lower
-	words = []
-	word = ""
-	for l in range(len(dirty)):
-		if dirty[l] != " ":
-			word += dirty[l]
-		else:
-			words.append(word)
-	return words
-
 class Piglatin:
-	consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
-	vowels = ["a", "e", "i", "o", "u"]
+	def __init__(self):
+		self.consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", 
+		"m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+		self.vowels = ["a", "e", "i", "o", "u"]
+
+	def write(self):
+		dirty = str(input('Input English Here: ')).lower().split(' ')
+		#Split input into array of words.
+		clean = ''
+		for l in range(len(dirty)):
+			conso = ''
+			if self.vowels.count(dirty[l][0]) >= 1:
+				clean += ''+dirty[l]+'yay '
+			for n in range(len(dirty[l])):
+				if self.consonants.count(dirty[l][n]) == 1:
+					conso += dirty[l][n]
+				else:
+					stem = str(dirty[l][n-1: -1]) + ''+conso+'ay'
+					if l != (len(dirty))-1:
+						clean += ''+stem+' '
+					else:
+						clean += ''+stem+'.'
+					break
+		return clean
 
 	def read(self):
 		build('Input Pig Latin Here:')
-		
 
-	def write(self):
-		build('Input English Here:')
+reader = Piglatin()
+print(reader.write())
