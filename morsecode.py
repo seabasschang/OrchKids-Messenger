06596daf3518e5e9280d18a05788d0b6c93ed2a3
cxiao18 <cxiao18@choate.edu>
2017-12-08 13:53:04 -0500

    get rid of unnecessary stuff

--- morsecode.py
+++ morsecode.py
@@ -1,60 +1,3 @@
-# #Goals: practice github and classes
-
-# #Messaging system: choose 3 outdated forms of messaging, write a class for each, write runner and filing classes
-# 	#Encrypted message, morse code, pig latin
-
-
-# #Structure: def translate, return translation, write(translate to) and read(translate back)
-
-
-# class MorseCode:
-# 	def __init__(self, message):
-# 		self.message = message
-# 	def encrypt(self, message):
-# 		encyrpted = [message]
-# 		#encrypted[len.encrypted-1] with for loop to access each element of the string
-
-# 		'a' = morse[0]
-# 		'b' = morse[1]
-# 		'c' = morse[2]
-# 		'd' = morse[3]
-# 		'e' = morse[4]
-# 		'f' = morse[5]
-# 		'g' = morse[6]
-# 		'h' = morse[7]
-# 		'i' = morse[8]
-# 		'j' = morse[9]
-# 		'k' = morse[10]
-# 		'l' = morse[11]
-# 		'm' = morse[12]
-# 		'n' = morse[13]
-# 		'o' = morse[14]
-# 		'p' = morse[15]
-# 		'q' = morse[16]
-# 		'r' = morse[17]
-# 		's' = morse[18]
-# 		't' = morse[19]
-# 		'u' = morse[20]
-# 		'v' = morse[21]
-# 		'w' = morse[22]
-# 		'x' = morse[23]
-# 		'y' = morse[24]
-# 		'z' = morse[25]
-
-# 		morse = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..']
-		
-# 		#use ord, minus ord of a,b,c,... to separate the letters in the string
-# 		#convert each letter into a numberical value
-# 		#assign the proper morse code to that value
-
-#Goals: practice github and classes
-
-#Messaging system: choose 3 outdated forms of messaging, write a class for each, write runner and filing classes
-	#Encrypted message, morse code, pig latin
-
-
-#Structure: def translate, return translation, write(translate to) and read(translate back)
-
 
 class MorseCode:
 	#def __init__(self, message):
@@ -102,7 +45,7 @@ class MorseCode:
 				'?': '••--••',
 				';': '-•-•-',
 				':': '---•••'}
-				
+
 		message = input('Message: ')
 		encoded=''
 		for i in message:
