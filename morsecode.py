d8bdeddd137b91a34025cf98c26decc62fdc2306
cxiao18 <34096914+cxiao18@users.noreply.github.com>
2017-12-07 21:50:13 -0500

    Add files via upload

--- morsecode.py
+++ morsecode.py
@@ -12,19 +12,41 @@ class MorseCode:
 		self.message = message
 	def encrypt(self, message):
 		encyrpted = [message]
-		encrypted[len.encrypted] 
+		#encrypted[len.encrypted-1] with for loop to access each element of the string
 
-		a = morse[0]
-		b = morse[1]
-		c = morse[2]
-		d = morse[3]
-		e = morse[4]
+		'a' = morse[0]
+		'b' = morse[1]
+		'c' = morse[2]
+		'd' = morse[3]
+		'e' = morse[4]
+		'f' = morse[5]
+		'g' = morse[6]
+		'h' = morse[7]
+		'i' = morse[8]
+		'j' = morse[9]
+		'k' = morse[10]
+		'l' = morse[11]
+		'm' = morse[12]
+		'n' = morse[13]
+		'o' = morse[14]
+		'p' = morse[15]
+		'q' = morse[16]
+		'r' = morse[17]
+		's' = morse[18]
+		't' = morse[19]
+		'u' = morse[20]
+		'v' = morse[21]
+		'w' = morse[22]
+		'x' = morse[23]
+		'y' = morse[24]
+		'z' = morse[25]
 
 		morse = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..']
 		
-
-
+		#use ord, minus ord of a,b,c,... to separate the letters in the string
 		#convert each letter into a numberical value
-		#use ord, minus ord of a,b,c,...
+		#assign the proper morse code to that value
+
+
 
  
