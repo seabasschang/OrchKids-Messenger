98cec4711c1efb580a313d3d5f3ad2120a1aa903
cxiao18 <cxiao18@choate.edu>
2017-12-08 13:43:48 -0500

    this works

--- morsecode.py
+++ morsecode.py
@@ -1,3 +1,52 @@
+# #Goals: practice github and classes
+
+# #Messaging system: choose 3 outdated forms of messaging, write a class for each, write runner and filing classes
+# 	#Encrypted message, morse code, pig latin
+
+
+# #Structure: def translate, return translation, write(translate to) and read(translate back)
+
+
+# class MorseCode:
+# 	def __init__(self, message):
+# 		self.message = message
+# 	def encrypt(self, message):
+# 		encyrpted = [message]
+# 		#encrypted[len.encrypted-1] with for loop to access each element of the string
+
+# 		'a' = morse[0]
+# 		'b' = morse[1]
+# 		'c' = morse[2]
+# 		'd' = morse[3]
+# 		'e' = morse[4]
+# 		'f' = morse[5]
+# 		'g' = morse[6]
+# 		'h' = morse[7]
+# 		'i' = morse[8]
+# 		'j' = morse[9]
+# 		'k' = morse[10]
+# 		'l' = morse[11]
+# 		'm' = morse[12]
+# 		'n' = morse[13]
+# 		'o' = morse[14]
+# 		'p' = morse[15]
+# 		'q' = morse[16]
+# 		'r' = morse[17]
+# 		's' = morse[18]
+# 		't' = morse[19]
+# 		'u' = morse[20]
+# 		'v' = morse[21]
+# 		'w' = morse[22]
+# 		'x' = morse[23]
+# 		'y' = morse[24]
+# 		'z' = morse[25]
+
+# 		morse = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..']
+		
+# 		#use ord, minus ord of a,b,c,... to separate the letters in the string
+# 		#convert each letter into a numberical value
+# 		#assign the proper morse code to that value
+
 #Goals: practice github and classes
 
 #Messaging system: choose 3 outdated forms of messaging, write a class for each, write runner and filing classes
@@ -8,45 +57,55 @@
 
 
 class MorseCode:
-	def __init__(self, message):
-		self.message = message
-	def encrypt(self, message):
-		encyrpted = [message]
-		#encrypted[len.encrypted-1] with for loop to access each element of the string
+	#def __init__(self, message):
+		#self•message = message
+	def encrypt():
+		morse = {'a': '•-', 
+				'b': '-•••', 
+				'c': '-•-•', 
+				'd': '-••', 
+				'e': '•', 
+				'f': '••-•', 
+				'g': '--•', 
+				'h': '••••', 
+				'i': '••', 
+				'j': '•---', 
+				'k': '-•-', 
+				'l': '•-••', 
+				'm': '--', 
+				'n': '-•', 
+				'o': '---', 
+				'p': '•--•', 
+				'q': '--•-', 
+				'r': '•-•', 
+				's': '•••', 
+				't':'-', 
+				'u': '••-', 
+				'v': '•••-', 
+				'w': '•--', 
+				'x': '-••-', 
+				'y': '-•--', 
+				'z': '--••',
+				' ': ' ',
+				'.': '.',
+				'1': '•----',
+				'2': '••---',
+				'3': '•••--',
+				'4': '••••-',
+				'5': '•••••',
+				'6': '-••••',
+				'7': '--•••',
+				'8': '---••',
+				'9': '----•',
+				'0': '-----'}
+		message = input('Message: ')
+		encoded=''
+		for i in message:
+			encoded+=(morse[i.lower()]+'/')
+		print(encoded)
 
-		'a' = morse[0]
-		'b' = morse[1]
-		'c' = morse[2]
-		'd' = morse[3]
-		'e' = morse[4]
-		'f' = morse[5]
-		'g' = morse[6]
-		'h' = morse[7]
-		'i' = morse[8]
-		'j' = morse[9]
-		'k' = morse[10]
-		'l' = morse[11]
-		'm' = morse[12]
-		'n' = morse[13]
-		'o' = morse[14]
-		'p' = morse[15]
-		'q' = morse[16]
-		'r' = morse[17]
-		's' = morse[18]
-		't' = morse[19]
-		'u' = morse[20]
-		'v' = morse[21]
-		'w' = morse[22]
-		'x' = morse[23]
-		'y' = morse[24]
-		'z' = morse[25]
 
-		morse = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..']
-		
-		#use ord, minus ord of a,b,c,... to separate the letters in the string
-		#convert each letter into a numberical value
-		#assign the proper morse code to that value
-
+MorseCode.encrypt()
 
 
  
