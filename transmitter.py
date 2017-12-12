db9649303fea9d343fec643698d368e89c24999d
cxiao18 <cxiao18@choate.edu>
2017-12-12 06:25:37 -0500

    typo it's ok

--- transmitter.py
+++ transmitter.py
@@ -1,6 +1,6 @@
 #Transmitter class
 class transmitTool:
-    #Initialiaze object with the name of the file, and the mode (read or write)
+    #Initialize object with the name of the file, and the mode (read or write)
     #The modes are the default python modes, so writing to a file is "w" and reading is "r"
     #Sets file object within transmitTool object
     def __init__(self, filename, mode):
